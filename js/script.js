f6cb40183e80468923604ea5a4e7dfe00189d0fc
edricjazmin <51036316+edricjazmin@users.noreply.github.com>
2019-11-04 13:36:48 -0500

    updated script.js and style.css
    -added functions in IIFE.
    -added more selectors to style.css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
 var tvRepo = (function () {
+  var repo = [];
+  var apiUrl = 'http://api.tvmaze.com/shows/431/episodes';
+
+  function add(episode) {
+    return repo.push(episode);
+  }
+
+  function getAll() {
+    return repo;
+  }
+
+  function loadList() {
+    
+  }
+
+  function loadDetails(item) {
+
+  }
+
+  function showDetails(item) {
+
+  }
+
+  function addListItem(episode) {
+
+  }
+
+  var $modalContainer = $('.modal-container');
+  function showModal() {
+
+  }
+
+  function hideModal() {
+
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    addListItem: addListItem
+  };
 
 })();
